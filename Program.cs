d3c776d7e0819ecdf924ed69383b556e3d837c47
FineAction <deleriour1@gmail.com>
2025-02-15 04:10:06 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿for (int value = 1; value < 25; value++)
+{
+    Console.WriteLine($"{value}{(IsEven(value) ? "even" : "odd")}");
+}
+ bool IsEven(int value)
+{
+    var IsEven = false;
+    if (value % 2 == 0)
+    {
+        IsEven = true;
+    }
+    else
+    {
+        IsEven = false;
+    }
+    return IsEven;
+}
+
+//Console.WriteLine($"{value}: {IsEven(value)}");
